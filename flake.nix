06cd5443cdba5ff8d0afb9fd2143e00a09d636bf
Maximilian Köhl <koehl@silitics.com>
2026-01-18 15:15:27 +0100

    add support for Nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          name = "nexigon";
+          src = ./.;
+          
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+        };
+      }
+    );
+}
